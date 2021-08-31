62c8048cc2370b97fe41a78ad07b2d9e4cb2cb7e
Lorenzo Balugani <lorenzo.balugani@gmail.com>
2021-08-31 14:32:52 +0200

    Create react app

--- erre2/database/db.py
+++ erre2/database/db.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:password@localhost:5432/Erre2"
+SQLALCHEMY_DATABASE_URL = "postgresql://postgres:password@localhost:5432/Erre2-Dev"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL
